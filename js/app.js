2ed316fbe91a94ecf005574a3d07e3f6a0cafafe
Tiberius <mercytiberius@yahoo.com>
2020-06-26 23:22:13 +0300

    add functionality

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const basketContainer = document.querySelector('.basket');
+const basketImage = document.querySelector('.basket img');
 let cursorPosition = window
 let screenWidth = window.innerWidth;
 console.log(cursorPosition)
@@ -6,6 +7,11 @@ console.log(screenWidth)
 
 const moveBasket = (e) => {
     console.log(e.screenX)
+    let randomMargin = Math.floor(Math.random() * screenWidth)
+
+    basketImage.style.right = `${randomMargin}px`;
+
+
 };
 
 basketContainer.onmouseover = moveBasket;
