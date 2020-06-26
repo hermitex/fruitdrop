4350506f93d3b776c26215ff1b9723535bd34afb
Tiberius <mercytiberius@yahoo.com>
2020-06-27 00:47:24 +0300

    fix

--- js/app.js
+++ js/app.js
@@ -1,13 +1,19 @@
 const controller = document.querySelector('.controller input');
 const basketImage = document.querySelector('.basket img');
 let cursorPosition = window
-let screenWidth = window.innerWidth;
-console.log(cursorPosition)
-console.log(controller.v)
 
 const moveBasket = (e) => {
-    console.log(e.target.value)
-    basketImage.style.right = `${e.target.value * 10}px`;
+    let screenWidth = window.innerWidth;
+    console.log(screenWidth)
+    if (screenWidth >= 450) {
+        console.log(screenWidth)
+        basketImage.style.right = `${e.target.value * 35}px`;
+    } else {
+        basketImage.style.right = `${e.target.value * 10}px`;
+    }
+
+
+
 
 };
 
