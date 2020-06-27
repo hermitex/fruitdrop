1885101c91b0a962d79d58e32d100728ec8743bd
Tiberius <mercytiberius@yahoo.com>
2020-06-27 11:05:03 +0300

    add controlls

--- js/app.js
+++ js/app.js
@@ -1,29 +1,19 @@
 const controller = document.querySelector('.controller input');
 const basketImage = document.querySelector('.basket img');
-let cursorPosition = window
+const gameItems = document.querySelectorAll('.game-item');
+
+
 
 const moveBasket = (e) => {
     let screenWidth = window.innerWidth;
-
-
     if (screenWidth >= 450) {
         console.log(screenWidth)
         basketImage.style.right = `${e.target.value * 36}px`;
     } else {
         basketImage.style.right = `${e.target.value * 10}px`;
     }
-
 };
 
-const changeBlinkRate = () => {
-    if (basketImage.classList.contains('blink-quick')) {
-        basketImage.classList.add('blink');
-        basketImage.classList.remove('blink-quick');
-    } else {
-        basketImage.classList.remove('blink-quick');
-        basketImage.classList.add('blink');
-    }
-};
+// gameItems.forEach(image => console.log(image))
 
 controller.oninput = moveBasket;
-controller.onchnage = changeBlinkRate;
