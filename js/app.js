1a4438d7e96fa2aa31cc9e16e2302db42a74a8ec
Tiberius <mercytiberius@yahoo.com>
2020-06-27 14:15:21 +0300

    fix controlls (interesting)

--- js/app.js
+++ js/app.js
@@ -28,13 +28,13 @@ const moveBasket = (e) => {
     let screenWidth = window.innerWidth;
     if (screenWidth >= 450) {
         console.log(screenWidth)
-        basketImage.style.right = `${e.target.value * 36}px`;
+        basketImage.style.right = `${-e.target.value * 36}px`;
     } else {
-        basketImage.style.right = `${e.target.value * 10}px`;
+        basketImage.style.right = `${-e.target.value * 10}px`;
     }
 };
 
-// gameItems.forEach(image => console.log(image))
+gameItems.forEach(image => console.log(image))
 
 controller.oninput = moveBasket;
 
