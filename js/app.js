9ea437e96357f7173063275e1ea4350b0026609b
Tiberius <mercytiberius@yahoo.com>
2020-06-27 12:08:43 +0300

    test controls

--- js/app.js
+++ js/app.js
@@ -1,7 +1,27 @@
 const controller = document.querySelector('.controller input');
 const basketImage = document.querySelector('.basket img');
 const gameItems = document.querySelectorAll('.game-item');
+const controls = document.querySelectorAll('.control');
 
+const playGame = (e) => {
+    if (e.target.classList.contains('play')) {
+        e.target.classList.remove('play')
+        e.target.classList.remove('fa-play');
+        e.target.classList.add('fa-pause');
+        gameItems.forEach(item => {
+            if (!item.classList.contains('game-item') && !e.target.classList.contains('play')) {
+                item.classList.add('game-item')
+            } else {
+                item.classList.remove('game-item')
+            }
+        });
+    } else {
+        e.target.classList.add('play')
+        e.target.classList.add('fa-play');
+        e.target.classList.remove('fa-pause');
+    }
+
+}
 
 
 const moveBasket = (e) => {
@@ -17,3 +37,8 @@ const moveBasket = (e) => {
 // gameItems.forEach(image => console.log(image))
 
 controller.oninput = moveBasket;
+
+controls.forEach(control => {
+    console.log(control)
+    control.onclick = playGame;
+});
